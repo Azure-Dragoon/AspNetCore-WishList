c2c5e20e39e4e137327365a8298b0d2f8f9f5267
Justin Tilton <jtilton@uwm.com>
2021-12-27 17:23:32 -0500

    create and delete actions added

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Item> model = new List<Item>();
+
+            foreach (Item item in _context.Items)
+            {
+                model.Add(item);
+            }
+            return View("Item", model);
+        }
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _context.Items.Remove(_context.Items.FirstOrDefault(x => x.Id == id));
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
