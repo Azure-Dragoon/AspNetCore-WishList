406656daafb5435472d59393aff1b64083bc568a
Justin Tilton <jtilton@uwm.com>
2021-12-27 16:17:54 -0500

    fix datatype for description

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -10,7 +10,7 @@ namespace WishList.Models
     {
         public int Id { get; set; }
         [MaxLength(50)]
-        public int Description  { get; set; }
+        public string Description  { get; set; }
 
     }
 }
